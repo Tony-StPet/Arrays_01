09dd2734e71631ab317f8983efab706a9d948ed1
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-18 10:23:30 +0300

    Update SortRoutines.java

--- src/SortRoutines.java
+++ src/SortRoutines.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class SortRoutines {
+public class SortRoutines {
+    // алгоритм сортировки выбором
+    public static void selectoinSort(int[] mas) {
+        int nMin;
+        for (int start = 0; start < mas.length-1; start++) {
+            // нахождение мин в части массива начиная со старт
+            nMin = start;
+            int min = mas[start];
+            for (int i = start + 1; i < mas.length; i++) {
+                if (mas[i] < min) {
+                    min = mas[i];
+                    nMin = i;
+                }
+                // change
+                if (nMin != start)
+                    ArrayRoutines.swapTwoElements(mas, nMin, start);
+            }
+
+        }
+    }
+
+    // Алгоритм сортировки пузырьком
+    public static void bubbleSort(int[] mas) {
+        for (int i = 0; i < mas.length-1; i++) {
+            int end = mas.length - i;
+            for (int j = 0; j < end-1; j++) {
+                if (mas[j] > mas[j+1])
+                    ArrayRoutines.swapTwoElements(mas, j, j+1);
+            }
+        }
+    }
+    //
+    // Алгоритм сортировки пузырьком С ФЛАГОМ (при сортированном массиве с незначит изменениями)
+    public static void bubbleSortF(int[] mas) {
+        boolean flag = true;
+        for (int i = 0; i < mas.length-1 && flag; i++) {
+            int end = mas.length - i;
+            flag = false;
+            for (int j = 0; j < end-1; j++) {
+                if (mas[j] > mas[j+1])
+                    ArrayRoutines.swapTwoElements(mas, j, j+1);
+                flag = true;
+            }
+        }
+    }
 }
