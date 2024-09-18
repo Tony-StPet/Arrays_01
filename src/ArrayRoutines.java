13eac32c5ade7aae73c766dd7d00a01b776057ca
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-18 10:22:09 +0300

    Update ArrayRoutines.java

--- src/ArrayRoutines.java
+++ src/ArrayRoutines.java
@@ -1,2 +1,82 @@
-package PACKAGE_NAME;public class ArrayRoutines {
+import java.util.Scanner;
+
+public class ArrayRoutines {
+    public static int[] inputArray(int size){
+        int[] mas = new int[size];
+        System.out.println("введите массив из "+size+" целых чисел");
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = scanner.nextInt();
+        }
+        return mas;
+    }
+
+    public static void printArray(int[] mas)    {
+        System.out.print("массив: ");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.print(" "+mas[i]);
+        }
+        System.out.println();
+    }
+
+    public static int min(int[] mas){
+        int m=mas[0];
+        for (int i = 1; i < mas.length; i++) {
+            if(m > mas[i])
+                m = mas[i];
+        }
+        return m;
+    }
+
+    //функция, которая находит максимум в массиве
+    public static int max(int[] mas){
+        int m=mas[0];
+        for (int i = 1; i < mas.length; i++) {
+            if(m < mas[i])
+                m = mas[i];
+        }
+        return m;
+    }
+    //функция, которая находит НОМЕР максимума
+    public static int indexOfMax(int[] mas){
+        int m=mas[0];
+        int numberOfMaximum = 0;
+        for (int i = 1; i < mas.length; i++) {
+            if(m < mas[i])
+            {
+                m = mas[i];
+                numberOfMaximum = i;
+            }
+        }
+        return numberOfMaximum;
+    }
+
+    public static double avg(int[] mas){
+        double av =0;
+        int sum =0;
+        for (int i = 0; i < mas.length; i++) {
+            sum +=mas[i];
+        }
+        av = ((double)sum)/mas.length;
+        return av;
+    }
+
+    public static void swapTwoElements(int[] mas, int i, int j){
+        int tmp = mas[i];
+        mas[i] = mas[j];
+        mas[j] = tmp;
+    }
+    public static void swapTwoElementsDouble(double[] mas, int i, int j){
+        double tmp = mas[i];
+        mas[i] = mas[j];
+        mas[j] = tmp;
+    }
+
+    public static int findX(int[] mas, int x)
+    {
+        for (int i = 0; i < mas.length; i++) {
+            if (x == mas[i]) return i;
+        }
+        return -1;  //дошли до конца массива, совпадений не было
+    }
 }
